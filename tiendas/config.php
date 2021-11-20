582ce350ccb9e9a1012a0407e9d5601962ac9daf
juan <juancavo19@gmail.com>
2021-11-20 13:06:34 -0500

    Servicios Web App

---
+++ tiendas/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$db = [
+    'host' => 'localhost',
+	'port'=> 5432,
+    'username' => 'postgres',
+	'password' => 'sebastian08',
+    'db' => 'gestionTienda' 
+];
+
+?>
---
+++ tiendas/post.php
@@ -0,0 +1,88 @@
+<?php
+include "config.php";
+include "utils.php";
+
+$dbConn =  connect($db);
+
+//MOSTRAR CON POST
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+    if (isset($_GET['CORREOPROPIETARIO']) && isset($_GET['CONTRASENAPROPIETARIO']))
+    {
+      //Mostrar un post
+      $sql = $dbConn->prepare("SELECT * FROM TBL_DATOSPROPIETARIO WHERE CORREOPROPIETARIO = :CORREOPROPIETARIO AND CONTRASENAPROPIETARIO = :CONTRASENAPROPIETARIO");
+      $sql->bindValue(':CORREOPROPIETARIO', $_GET['CORREOPROPIETARIO']);
+	  $sql->bindValue(':CONTRASENAPROPIETARIO', $_GET['CONTRASENAPROPIETARIO']);
+      $sql->execute();
+      header("HTTP/1.1 200 OK");
+      echo json_encode(  $sql->fetch(PDO::FETCH_ASSOC)  );
+      exit();
+	  }
+
+	  else {
+      //Mostrar lista de post
+      $sql = $dbConn->prepare("SELECT * FROM TBL_DATOSPROPIETARIO");
+      $sql->execute();
+      $sql->setFetchMode(PDO::FETCH_ASSOC);
+      header("HTTP/1.1 200 OK");
+      echo json_encode( $sql->fetchAll()  );
+      exit();
+	}
+}
+
+//INSERTAR SE HACE CON POST
+/*
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $input = $_POST;
+    $sql = "INSERT INTO estudiante
+          (codigo, nombre, apellido, edad)
+          VALUES
+          (:codigo, :nombre, :apellido, :edad)";
+    $statement = $dbConn->prepare($sql);
+    bindAllValues($statement, $input);
+    $statement->execute();
+
+    $postCodigo = $dbConn->lastInsertId();
+    if($postCodigo)
+    {
+      $input['codigo'] = $postCodigo;
+      header("HTTP/1.1 200 OK");
+      echo json_encode($input);
+      exit();
+	 }
+ }
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'DELETE')
+{
+	$codigo = $_GET['codigo'];
+  $statement = $dbConn->prepare("DELETE FROM  estudiante where codigo=:codigo");
+  $statement->bindValue(':codigo', $codigo);
+  $statement->execute();
+	header("HTTP/1.1 200 OK");
+	exit();
+}
+
+//ACTUALIZAR CON PUT
+if ($_SERVER['REQUEST_METHOD'] == 'PUT')
+{
+    $input = $_GET;
+    $postCodigo = $input['codigo'];
+    $fields = getParams($input);
+
+    $sql = "
+          UPDATE estudiante
+          SET $fields
+          WHERE codigo='$postCodigo'
+           ";
+
+    $statement = $dbConn->prepare($sql);
+    bindAllValues($statement, $input);
+
+    $statement->execute();
+    header("HTTP/1.1 200 OK");
+    exit();
+}*/
+
+?>
